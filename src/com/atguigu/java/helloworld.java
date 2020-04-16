314c4eb6dab79f819aefac386d0f7121333f5fcd
hhuan019 <hhuan019@fiu.edu>
2020-04-16 17:20:03 -0400

    Initial commit

---
+++ src/com/atguigu/java/helloworld.java
@@ -0,0 +1,27 @@
+package com.atguigu.java;
+
+import java.util.Date;
+
+public class helloworld {
+
+    public static void main(String[] args){
+
+        System.out.println("helloworld!");
+        new Date();
+        System.out.println("helloworld!");
+
+
+        System.out.println("helloworld!");
+
+
+
+
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");;
+
+
+
+    }
+
+}
---
+++ src/com/atguigu/java/helloworld1.java
@@ -0,0 +1,9 @@
+package com.atguigu.java;
+
+/**
+ * @author Hao Huang
+ * @create 2020-04-16 13:52
+ */
+public class helloworld1 {
+
+}
